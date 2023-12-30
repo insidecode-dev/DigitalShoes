9caae9b25957579fe6a0b866748d51a914f7703b
insidecode-dev <trqtmhdv@gmail.com>
2023-12-30 15:33:20 +0400

    final project for matrix training center

--- DigitalShoes.Domain/Entities/PaymentObject.cs
+++ DigitalShoes.Domain/Entities/PaymentObject.cs
@@ -3,7 +3,7 @@
     public class PaymentObject:BaseEntity
     {
         public int ShoeId { get; set; }        
-        public int PaymentId { get; set; }
+        public int PaymentId { get; set; } // where is shoe
         public Payment Payment { get; set; }
         public int ItemsCount { get; set; }
         public decimal Price { get; set; }
